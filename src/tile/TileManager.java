36e91a65e62a8ea9e47e84439d3318bfca7e1fb3
TheonlyTazz <TheonlyTazz@gmail.com>
2023-01-18 07:15:36 +0100

    Map work

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,38 +4,76 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tile;
+    public static final String fileName = ( "/maps/map_1.txt");
+    int[][] mapTileNum;
 
     public TileManager(GamePanel gp) {
 
         this.gp = gp;
-
         tile = new Tile[10];
+        mapTileNum = new int [gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
+        loadMap(fileName);
     }
 
     public void getTileImage() {
-
+        System.out.println("Image loading started");
         try {
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+            tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/grass.png")));
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+            tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/wall.png")));
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
-
+            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("tiles/water.png")));
 
         }catch(IOException e){
             e.printStackTrace();
         }
+        System.out.println("Image loading finished");
     }
+    public void loadMap(String filePath) {
+        try {
+            //InputStream is = new FileInputStream("../../maps/map01.txt");
+            InputStream is = Objects.requireNonNull(getClass().getResourceAsStream(filePath));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+                String line = br.readLine();
+                while (col < gp.maxScreenCol) {
+
+                    String[] numbers = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]);
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+
+            }
+            br.close();
+        }catch(Exception e) {
+            System.out.println("Cant load Map!");
+
+        }
+
+    }
     public void draw(Graphics2D g2) {
 
         int col = 0;
@@ -45,7 +83,9 @@ public class TileManager {
 
         while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
 
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
             col++;
             x += gp.tileSize;
             if(col == gp.maxScreenCol) {
