985f0781a74d6b6a15e83abb2b96f9f168eaa1e7
TheonlyTazz <TheonlyTazz@gmail.com>
2023-02-08 21:39:16 +0100

    Custom spreadsheets

--- res/tiled/Modern_Exteriors_Complete_Tileset.tsx
+++ res/tiled/Modern_Exteriors_Complete_Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="36432" columns="176">
- <image source="Modern_Exteriors_Complete_Tileset.png" trans="ff00ff" width="2816" height="3312"/>
+ <image source="../spritesheets/Modern_Exteriors_Complete_Tileset.png" trans="000000" width="2816" height="3312"/>
 </tileset>
